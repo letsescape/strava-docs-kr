920de914099190eaee416ad644511d8fe45a9d08
LeeByeongMuk <mansogod@gmail.com>
2022-01-08 14:10:49 +0900

    test md render

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,17 +1,29 @@
-import React from "react";
-import Logo from '../images/strava_logo_nav.png';
+import React, {useEffect} from "react";
+import {Link} from "react-router-dom";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+
+import Logo from "../images/strava_logo_nav.png";
+import test from "../markdown/test.md";
 
 export default function Header() {
+    useEffect(() => {
+        axios(test).then(data => console.log(data.data));
+    })
+
     return (
         <header>
             <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse">
                 <div className="container">
-                    <a className="navbar-brand" href="/">
+                    <Link
+                        to="/"
+                        className="navbar-brand"
+                    >
                         <span className="strava-logo">
                             <img src={Logo} alt="Strava"/>
                         </span>
                         <span className="strava-subbranding">Developers</span>
-                    </a>
+                    </Link>
                 </div>
             </nav>
         </header>
