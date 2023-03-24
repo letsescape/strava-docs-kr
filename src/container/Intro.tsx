03b19243f668a0501afb27232a27562c1df526b1
kimchanhyung98 <kimchanhyung98@gmail.com>
2023-03-25 01:28:24 +0900

    #2 Update intro

--- src/container/Intro.tsx
+++ src/container/Intro.tsx
@@ -43,20 +43,21 @@ export default function Intro() {
     )
 }
 
-const INTRO_TITLE = 'The Strava API';
+const INTRO_TITLE = '스트라바 API';
 const INTRO_DESCRIPTIONS = [
-    'Strava athletes upload millions of activities every day. Our open API and this rich data set yield diverse opportunities for developers, from creating new hardware to augmenting the Strava experience.',
-    `he Strava V3 API is a publicly available interface that allows developers to access Strava data. The interface is stable and used by the Strava mobile apps. However, we occasionally make major changes to improve performance and enhance our features (<a href="/docs/changelog">see the changelog for more details</a>).`,
-    `Strava reserves the right to revoke your API Token if you violate the <a href="https://www.strava.com/legal/api">API Agreement</a>, including but not limited to, uses that enable virtual races or competitions and uses that replicate Strava sites, services or products.`
+    '스트라바의 운동 선수들은 매일 수백만 개의 활동을 업로드합니다. 우리의 개방형 API와 풍부한 데이터는 새로운 하드웨어를 만들거나 스트라바 경험을 보완하는 등 다양한 개발 기회를 제공합니다.',
+    '스트라바 V3 API는 개방형 API로 개발자가 스트라바 데이터에 액세스할 수 있습니다. 이 인터페이스는 안정적이며 스트라바 모바일 앱에서 사용됩니다.',
+    `그러나 때로는 성능 개선과 기능 추가를 위해 중대한 변경 사항이 발생하기도 합니다. (<a href="https://developers.strava.com/docs/changelog/">자세한 내용은 변경 로그를 참조하세요</a>)`,
+    `만약 가상 경기, 대회나 스트라바 사이트나 서비스를 복제하는 경우를 포함하여  <a href="https://www.strava.com/legal/api">API 약관</a>을 위반하면 스트라바는 API 토큰을 취소할 권리를 보유합니다.`
 ];
 const INTRO_LINKS = [
     {
         isLink: true,
-        href: '/docs/getting-started',
-        isRoute: true,
+        href: 'https://developers.strava.com/docs/getting-started',
+        isRoute: false,
         theme: 'primary',
         style: 'block',
-        text: 'Getting Started Guide'
+        text: '시작 가이드'
     },
     {
         isLink: true,
@@ -64,7 +65,7 @@ const INTRO_LINKS = [
         isRoute: true,
         theme: 'secondary',
         style: 'block',
-        text: 'API Documentation'
+        text: 'API 문서'
     },
     {
         isLink: true,
@@ -72,15 +73,15 @@ const INTRO_LINKS = [
         isRoute: false,
         theme: 'secondary',
         style: 'block',
-        text: 'Create & Manage Your App'
+        text: '앱 생성 & 관리'
     },
     {
         isLink: true,
-        href: '/playground',
-        isRoute: true,
+        href: 'https://developers.strava.com/playground',
+        isRoute: false,
         theme: 'secondary',
         style: 'block',
-        text: 'Explore the API Playground'
+        text: 'Swagger'
     },
     {
         isLink: true,
@@ -88,16 +89,20 @@ const INTRO_LINKS = [
         isRoute: false,
         theme: 'secondary',
         style: 'block',
-        text: 'Libraries'
+        text: '라이브러리'
     },
 ]
 
-const BANNER_TITLE = 'Strava Apps';
-const BANNER_DESCRIPTION = `Thousands of amazing developers from all over the world power their apps with Strava data. Far more athletes are using those apps to augment their Strava experience. There’s an app for everyone, from those that let you dive deep into the nerdiest of performance data, to an app that helps you make a friend in your neighborhood who runs the same pace as you.`;
+const BANNER_TITLE = '스트라바 앱';
+const BANNER_DESCRIPTION = [
+    '전 세계의 수많은 개발자들이 스트라바 데이터를 활용하여 자신의 앱을 개발하고 있습니다.',
+    '이제는 더 많은 운동 선수들이 앱을 통해 스트라바 더욱 즐기고 있습니다.',
+    '성능 데이터 분석에 깊이 들어갈 수 있는 앱에서부터 이웃과 함께 달릴 수 있는 앱까지, 모든 운동 선수들을 위한 앱이 있습니다.'
+];
 const BANNER_LINK = {
     isLink: true,
     href: 'https://www.strava.com/apps',
     isRoute: false,
     theme: 'secondary',
-    text: 'View the App Directory'
+    text: '앱 디렉토리'
 };
